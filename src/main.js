03bd80716880c048f1d07a7f189770de1377948e
Ashkaan Jaberi <ashkaan14@gmail.com>
2017-12-13 19:18:23 +0000

    boiler plate all set up

--- src/main.js
+++ src/main.js
@@ -2,4 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
+import App from './component/app';
 
+import './style/main.scss';
+
+console.log('hello');
+
+const container = document.createElement('div');
+document.body.appendChild(container);
+ReactDOM.render(<App />, container);
