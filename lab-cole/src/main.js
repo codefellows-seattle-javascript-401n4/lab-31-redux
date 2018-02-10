dd2d9ee55313041196539e1bbaa46836905402ba
Cole Allen <colesss>
2018-02-09 17:54:28 -0800

    started over with starter code, developer code pending

--- lab-cole/src/main.js
+++ lab-cole/src/main.js
@@ -1,38 +1,22 @@
-'use strict';
+import './style/main.scss'
+import React from 'react'
+import ReactDom from 'react-dom'
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
 
-//css, why are we not declaring this path the same way as the compenent/app paths?
-import './style/main.scss';
-//app compontents
-import App from './components/app.js';
-import createStore from './app/store.js';
+import App from './component/app'
+import sectionsReducer from './reducer/sections.js'
 
+let store = createStore(sectionsReducer)
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {BrowserRouter} from 'react-router-dom'
-import {Provider} from 'react-redux';
+store.subscribe(() => {
+  console.log('__STATE__', store.getState())
+})
 
-const store = createStore();
-
-class Main extends React.Component {
-
-  constructor(props) {
-    super(props);
-  }
-
-  componentDidMount() {
-    store.subscribe( () => console.log("__STORE__", store.getState()) );
-  }
-
-  render() {
-    return (
-      <Provider store={store}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </Provider>
-    )
-  }
-}
-
-ReactDom.render(<Main/>, document.getElementById('root'));
+const container = document.createElement('div')
+document.body.appendChild(container)
+ReactDom.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, 
+container)
--- lab-cole/webpack.config.js
+++ lab-cole/webpack.config.js
@@ -1,64 +1,46 @@
 'use strict';
 
-// Dynamic Script and Style Tags
 const HTMLPlugin = require('html-webpack-plugin');
-
-// Makes a separate CSS bundle
-const ExtractPlugin = require ('extract-text-webpack-plugin');
+const ExtractPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  
-  //Load this and everything it cares about
+  devtool: 'cheap-eval-source-map',
   entry: `${__dirname}/src/main.js`,
-  
   devServer: {
-    historyApiFallback:true,
+    historyApiFallback: true,
   },
-  
-  devTool: 'source-map',
-
-  // Stick it into the "path" folder with that file name
   output: {
     filename: 'bundle.[hash].js',
     path: `${__dirname}/build`,
   },
-
   plugins: [
-    new HTMLPlugin({
-      template:`${__dirname}/src/index.html`,
-    }),
+    new HTMLPlugin({title: 'kanban'}),
     new ExtractPlugin('bundle.[hash].css'),
   ],
-
   module: {
     rules: [
-      // If it's a .js file not in node_modules, use the babel.loader
       {
-        test: /\.scss$/,
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
       },
-      // If it's a .scss file 
       {
         test: /\.scss$/,
         loader: ExtractPlugin.extract({
-          // These get loaded in reverse order and the output of one pipes into the other (think of a then)
           use: [
-            {
-              loader: 'css-loader', 
-              options: {
-                sourceMap:true,
-              },
-            },
+            'css-loader',
             'resolve-url-loader',
             {
               loader: 'sass-loader',
               options: {
                 sourceMap: true,
-                includePaths:[`${__dirname}/src/style`],
+                includePaths: [`${__dirname}/src/style`],
               },
             },
           ],
         }),
       },
+
     ],
   },
 };
